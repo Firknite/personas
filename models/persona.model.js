388824cd87f5a6385b2c573d0e1d8eaa9a7dc14c
Rigoberto Torres <backlinemrc2019@gmail.com>
2020-04-04 22:36:32 -0300

    pruebas unitarias modelo persona

--- models/persona.model.js
+++ models/persona.model.js
@@ -1,30 +1,40 @@
 var personas = [
-	{ id: 1, nombre: 'Juan', edad: 30 },
-	{ id: 2, nombre: 'Maria', edad: 25 },
-	{ id: 3, nombre: 'Pedro', edad: 50 },
+  { id: 1, nombre: 'Juan', edad: 30 },
+  { id: 2, nombre: 'Maria', edad: 25 },
+  { id: 3, nombre: 'Pedro', edad: 50 }
 ];
 
-const listarPersonas = (id) => {
-	if (id !== undefined && id !== null) {
-		const filtrado = personas.filter((persona) => persona.id == id);
-		return filtrado;
-	} else {
-		return personas;
-	}
+const listarPersonas = id => {
+  if (id !== undefined && id !== null) {
+    const filtrado = personas.filter(persona => persona.id == id);
+    console.log({filtrado});
+    return filtrado;
+  } else {
+	console.log({personas});
+    return personas;
+  }
 };
 
-const agregarPersonas = (persona) => {
-	persona.id = maxId() + 1;
-	personas = [...personas, persona];
-	return personas;
+const agregarPersonas = persona => {
+  persona.id = maxId() + 1;
+  personas = [...personas, persona];
+  return personas;
 };
 
 const maxId = () => {
-	let maxId = 0;
-	personas.reduce((acc, act) => {
-		if (act.id > maxId) maxId = act.id;
-	}, 0);
-	return maxId;
+  let maxId = 0;
+  personas.reduce((acc, act) => {
+    if (act.id > maxId) maxId = act.id;
+  }, 0);
+  return maxId;
 };
+
+const autenticar = (num) => {
+	if(num>100) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 module.exports = { listarPersonas, agregarPersonas };
--- test/persona.spec.js
+++ test/persona.spec.js
@@ -1,7 +1,27 @@
-function sum(a, b) {
-	return a + b;
-}
+// mis funciones basicas de jest
 
-test('test para funcion suma', () => {
-	expect(sum(1, 2)).toBe(3);
+// function sum(a, b) {
+// 	return a + b;
+// }
+
+// test('test para funcion suma', () => {
+// 	expect(sum(1, 2)).toBe(3);
+// });
+
+const { listarPersonas } = require('../models/persona.model');
+
+test('listar personas con id', () => {
+  const esperado = [{ id: 1, nombre: 'Juan', edad: 30 }];
+  const resultado = listarPersonas(1);
+  expect(resultado).toStrictEqual(esperado);
+});
+
+test('listar personas', () => {
+  const esperado = [
+    { id: 1, nombre: 'Juan', edad: 30 },
+    { id: 2, nombre: 'Maria', edad: 25 },
+    { id: 3, nombre: 'Pedro', edad: 50 }
+  ];
+  const resultado = listarPersonas();
+  expect(resultado).toStrictEqual(esperado);
 });
